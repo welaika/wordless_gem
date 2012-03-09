c1abb54710ad4c86509181804ea87695aa08b6f6
Étienne Després <etienne.despres@molotovcoop.org>
2012-03-09 00:06:53 -0500

    First working version of the executable.

--- lib/wordless/commands.rb
+++ lib/wordless/commands.rb
@@ -1,10 +1,16 @@
+require 'rubygems'
 require 'thor'
 
 module Wordless
   class Commands < Thor
+    
     desc "new SITE_NAME", "Creates a new Wordless-enabled WordPress installation with a pre-configured Wordless theme"
     def new(name)
-      `curl -O http://wordpress.org/latest.tar.gz && tar -xzvf latest.tar.gz -C #{name}`
+      puts "Downloading the latest stable version of WordPress"
+      `mkdir #{name} && curl -O http://wordpress.org/latest.tar.gz && tar -xzvf latest.tar.gz -C #{name}`
     end
+    
   end
+  
+  Commands.start
 end
