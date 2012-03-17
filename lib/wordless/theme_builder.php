f37dd52ef8efa9baae538676d7b656d039e6d08d
Étienne Després <etienne.despres@molotovcoop.org>
2012-03-17 19:09:43 -0400

    Fix permissions bug.

--- lib/wordless/theme_builder.php
+++ lib/wordless/theme_builder.php
@@ -7,15 +7,14 @@ require_once "$plugin_dir/wordless/wordless.php";
 require_once Wordless::join_paths($plugin_dir, "wordless", "theme_builder.php");
 
 $theme_name = $argv[1] ? $argv[1] : 'wordless';
-// $permissions = substr(sprintf('%o', fileperms($plugin_dir)), -4);
-$permissions = '0664';
+$permissions = substr(sprintf('%o', fileperms($plugin_dir)), -4);
 
 # Required WordPress function
 function get_template_directory() {
   return Wordless::join_paths($current_dir, "wp-content", "themes", "whatever");
 }
 
-$builder = new WordlessThemeBuilder($theme_name, $theme_name, $permissions);
+$builder = new WordlessThemeBuilder($theme_name, $theme_name, intval($permissions, 8));
 $builder->build();
 
 ?>
