302503bdbdcf26d3d4b90ed31b940024c90716b6
Étienne Després <etienne.despres@molotovcoop.org>
2012-03-17 18:58:43 -0400

    Fix typo.

--- lib/wordless/theme_builder.php
+++ lib/wordless/theme_builder.php
@@ -8,7 +8,7 @@ require_once Wordless::join_paths($plugin_dir, "wordless", "theme_builder.php");
 
 $theme_name = $argv[1] ? $argv[1] : 'wordless';
 // $permissions = substr(sprintf('%o', fileperms($plugin_dir)), -4);
-$persmissions = '0664'
+$persmissions = '0664';
 
 # Required WordPress function
 function get_template_directory() {
