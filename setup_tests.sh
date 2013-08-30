a440f38920f7bf6afb0fc8eca58308e867a2b773
Ju Liu <ju.liu@welaika.com>
2013-08-30 18:12:08 +0200

    fixed test setup script

--- setup_tests.sh
+++ setup_tests.sh
@@ -8,6 +8,6 @@ rm -rf $FIXTURE_PATH
 
 git clone --branch=master git://github.com/welaika/wordless.git $FIXTURE_PATH && cd $FIXTURE_PATH
 cp -f ../wordless_preferences.php wordless/theme_builder/vanilla_theme/config/initializers/wordless_preferences.php
-git ci -am "updated ruby and compass path"
+git commit -am "updated ruby and compass path"
 
 echo "[OK!]"
