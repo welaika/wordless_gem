66c5c3b064287313def70e055ad31c1d35208a82
Ju Liu <ju.liu@welaika.com>
2013-08-30 18:23:37 +0200

    added default git mail in test setup

--- setup_tests.sh
+++ setup_tests.sh
@@ -10,6 +10,7 @@ git clone --branch=master git://github.com/welaika/wordless.git $FIXTURE_PATH &&
 cp -f ../wordless_preferences.php wordless/theme_builder/vanilla_theme/config/initializers/wordless_preferences.php
 
 git config user.name "Wordless Tester"
+git config user.email "tester@wordless.com"
 git commit -am "updated ruby and compass path"
 
 echo "[OK!]"
